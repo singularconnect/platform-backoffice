5a536b4da75a189e5155abc8c774f92cb0cc4638
Bruno Jorge Kosloski <brunojorgek@gmail.com>
2016-08-18 16:04:55 -0300

    rethinkdb config ready for replicas and shards

---
+++ config/rethinkdb.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'clustering' => env('APP_ENV') != 'production' and env('APP_ENV') != 'staging' ?
+        'default' : [
+        // configs per table - based on https://www.rethinkdb.com/api/javascript/reconfigure/
+        'all_tables' => [
+            'shards' => 1,
+            'replicas' => [
+                'sb_platform' => 1
+            ],
+            'primaryReplicaTag' => 'sb_platform'
+        ]
+    ]
+
+];
