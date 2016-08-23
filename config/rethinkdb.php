86b20a6bcc60743dedb8fee4ae98b763ec0df288
Bruno Jorge Kosloski <brunojorgek@gmail.com>
2016-08-23 18:37:22 -0300

    configs rethink clustering fix with snake case again

--- config/rethinkdb.php
+++ config/rethinkdb.php
@@ -2,17 +2,17 @@
 
 return [
 
-    'clustering' => (env('APP_ENV') == 'production' and env('APP_ENV') == 'staging') ?
+    'clustering' => (env('APP_ENV') != 'production' and env('APP_ENV') != 'staging') ?
         'default' : [
-        // configs per table - based on https://www.rethinkdb.com/api/javascript/reconfigure/
-        'all_tables' => [
-            'shards' => 1,
-            'replicas' => [
-                'default' => 1
-            ],
-            // primaryReplicaTag is the original docs/examples but with php is snake_case
-            'primary_replica_tag' => 'default'
+            // configs per table - based on https://www.rethinkdb.com/api/javascript/reconfigure/
+            'all_tables' => [
+                'shards' => 1,
+                'replicas' => [
+                    'sb_platform' => 1
+                ],
+                // primaryReplicaTag is the original docs/examples but with php is snake_case
+                'primary_replica_tag' => 'sb_platform'
+            ]
         ]
-    ]
 
 ];
